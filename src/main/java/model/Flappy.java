b1d44908cc13a9d91a7f07b965a7264ebde15e2b
davide.ciandrini <davide.ciandrini@studio.unibo.it>
2020-05-09 17:09:03 +0200

    create a prototype of flappy

---
+++ src/main/java/model/Flappy.java
@@ -0,0 +1,12 @@
+package model;
+
+public interface Flappy {
+
+    int getWidthBird();
+    int getHeightBird();
+    double getPosX();
+    void setPosX(double posX);
+    double getPosY();
+    void setPosY(double posY);
+
+}
